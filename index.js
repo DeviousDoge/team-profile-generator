0f39cd9cb5ca7fe854198e95164bb698f7397397
Sean Dillon <pdillon0@gmail.com>
2022-08-13 13:51:18 -0400

    packets and init

---
+++ index.js
@@ -0,0 +1,83 @@
+const { type } = require("os");
+
+function init() {
+    // declare vars
+    const inquirer = require("inquirer");
+    const fs = require("fs");
+    const Employee = require("./lib/Employee.js");
+    const Engineer = require("./lib/Engineer.js");
+    const Intern = require("./lib/Intern.js");
+    const Manager = require("./lib/Manager.js");
+    const generateHTML = require("./src/generateHTML.js");
+    //prompt chain
+    const employeeQuestion = [
+        {
+            type: "list",
+            name: "employeeTitle",
+            message: "What's the employee's title",
+            choices: ["Engineer", "Intern"]
+
+        },
+        //when role is engineer is true, ask this question
+        {
+            when: input => {
+                return input.employeeTitle == "Engineer"
+            },
+            type: "input",
+            name: "github",
+            message: "Enter your github username:",
+        },
+        //when role is intern is true, ask this question
+        {
+            when: input => {
+                return input.employeeTitle == "Intern"
+            },
+            type: "input",
+            name: "school",
+            message: "What's the school you enrolled in ?",
+        },
+        
+    ];
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What's the name of the manager?",
+                name: "mgrName"
+            },
+            {
+                type: "input",
+                message: "What's the managers id?",
+                name: "mgrID"
+            },
+            {
+                type: "input",
+                message: "Whats the managers email?",
+                name: "mgrEmail"
+            }
+
+        ]
+        )
+        .then(function (answers) {
+            const addEmployee =  inquirer.prompt ([
+                {
+                    type: "confirm",
+                    name: "addAnotherEmployee",
+                    message: "Add another employee?",
+                }, {
+                    when: response => {
+                      return response.addAnotherEmployee == true
+                    },
+                    type: "input",
+                    name: "employeeName",
+                    message: "What is the employees name?"
+                }]);
+            addEmployee;
+            console.log(answers)
+        }
+        )
+
+
+}
+//initialize app
+init();
